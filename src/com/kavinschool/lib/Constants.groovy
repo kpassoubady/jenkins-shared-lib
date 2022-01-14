8c1ca0f2ed82f86a1d3e9070b1fc4083c294bf02
kangs passoubady <kpassoubady@gmail.com>
2022-01-13 21:37:20 -0800

    changed type

--- src/com/kavinschool/lib/Constants.groovy
+++ src/com/kavinschool/lib/Constants.groovy
@@ -5,7 +5,7 @@ package com.kavinschool.lib
  */
 class Constants {
 
-    public static final BigDecimal PI = 3.141592653589793
+    public static final String PI = '3.141592653589793'
     public static final String GRID_HUB = 'http://localhost:4445/wd/hub'
 
 }
